f0e08ef245508d76d3823a29a0f8d04199391bab
Dhruv <dhruvjaindj888@gmail.com>
2025-02-28 23:56:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+/draw-app
+  ├── /src
+  │   ├── /components
+  │   │   ├── Toolbar.jsx
+  │   │   ├── Canvas.jsx
+  │   │   ├── GridOverlay.jsx
+  │   ├── App.jsx
+  │   ├── main.jsx
+  ├── /public
+  ├── index.html
+  ├── package.json
+  ├── vite.config.js# Draw_App
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Canvas from "./components/Canvas";
+import './App.css';
+
+function App() {
+  // const [tool, setTool] = useState("pen");
+  // const [gridVisible, setGridVisible] = useState(false);
+
+  return (
+    <Canvas />
+  );
+}
+
+export default App;
